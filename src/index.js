5c707ea4532009f1e2c0145c88ffd19359671a72
RudeGames <fs_boukhoulda@esi.dz>
2018-07-16 19:24:13 +0100

    Added search_bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAjYICaM48RA5slVzbGVrkOMS_-WRyG46c';
 // Create the Component
 const App = () => {
-    return <div>Hi!</div>;
+    return <div>
+        <SearchBar />
+    </div>;
 }
 
 //Pass the componenet to DOM
