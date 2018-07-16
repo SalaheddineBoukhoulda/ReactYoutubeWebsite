c0024cd8858b128b5ba34ab6c89599f59c820285
RudeGames <fs_boukhoulda@esi.dz>
2018-07-16 22:59:17 +0100

    Parent component refactor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import YSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAjYICaM48RA5slVzbGVrkOMS_-WRyG46c';
+
+
 // Create the Component
-const App = () => {
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos : []};
+        //Youtube query fetch
+        YSearch({key:API_KEY,term:'football'},function(videos){
+            this.setState({videos});
+        });
+    }
+    render(){
     return <div>
         <SearchBar />
     </div>;
+    }
 }
 
 //Pass the componenet to DOM
