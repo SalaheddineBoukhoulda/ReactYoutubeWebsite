8d8cda2e0a4e5fc506ff27cb7ab5e89cd08244b1
RudeGames <fs_boukhoulda@esi.dz>
2018-07-17 01:04:53 +0100

    Added video list iterator

---
+++ src/components/video_list_item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import VideoList from './video_list';
+
+const VideoListItem = (props) => {
+    return (
+        <li>Video</li>
+    )
+};
+
+
+export default VideoListItem;
